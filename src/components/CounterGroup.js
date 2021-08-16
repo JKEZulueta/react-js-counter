054f38ef7542688673b3072a741b5ce6d3fb258e
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-16 19:59:17 +0800

    [Counter][ZULUEJA]feat: Added Counter Group

---
+++ src/components/CounterGroup.js
@@ -0,0 +1,16 @@
+import Counter from "./Counter";
+
+function CounterGroup (){
+
+    return(
+        <div>
+        <Counter></Counter>
+        <Counter></Counter>
+        <Counter></Counter>
+        <Counter></Counter>
+        </div>
+    );
+
+}
+
+export default CounterGroup;
